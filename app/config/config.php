77d8b8bd79991f4d6dedeeecaf73e1752fd87ebf
Ali Rahemi <deutschlandmyhome@gmail.com>
2019-12-04 14:29:05 +0100

    fix URLADDRESS & DB Setting  in Config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
     // DB Params
     define('DB_HOST', '127.0.0.1');
-    define('DB_USER', 'ali');
-    define('DB_PASS', 'ali123456');
+    define('DB_USER', 'root');
+    define('DB_PASS', '123456');
     define('DB_NAME', 'shareposts');
     
     // App Root
     define ('APPROOT', dirname(dirname (__FILE__)));
 
     // URL Root
-    define ('URLROOT', 'http://localhost:3000/shareposts');
+    define ('URLROOT', 'http://localhost/shareposts');
 
     // Site Name
     define('SITENAME', 'SharePosts');
